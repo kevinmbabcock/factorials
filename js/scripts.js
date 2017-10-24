9f60456dee0e989ec056da3951f66fb9483039f7
Kevin Babcock and Malgorzata Haniszewska <kevin@>
2017-10-24 14:13:02 -0700

    ask user for a number and display the factorial of that number

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var factorial = function(number) {
+  var total = 1;
+  for (index = number; index > 1; index -=2) {
+    var total = total * (number * (index-1));
+
+    number -=2;
+
+  };
+  return total;
+}
+
+$(document).ready(function() {
+
+  $("#userInput").submit(function(event) {
+    event.preventDefault();
+    $(".hide").show();
+    var outputNumber = parseInt($("#userNumber").val());
+    $(".show").text(factorial(outputNumber));
+    //alert(factorial(outputNumber));
+  })
+})
